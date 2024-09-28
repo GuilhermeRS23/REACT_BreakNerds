716a9cb1702c7e3585329b9a71e1f066a53d043c
Guilherme R.Silva <guilhermemuralha2016@gmail.com>
2024-09-28 11:03:13 -0300

    #60. Cabeçalho do perfil de usuário

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,10 +1,28 @@
 import { useContext } from "react";
 import { UserContext } from "../../Context/UserContext";
+import { ProfileAction, ProfileAddIcon, ProfileAvatar, ProfileBackground, ProfileContainer, ProfileEditIcon, ProfileHeader, ProfileUser } from "./ProfileStyled";
 
 const Profile = () => {
     const { user } = useContext(UserContext);
     return (
-        <h1>{user.name}</h1>
+        <ProfileContainer>
+            <ProfileHeader>
+                <ProfileEditIcon>
+                    <i className="bi bi-pencil-square"></i>
+                </ProfileEditIcon>
+                <ProfileBackground src={user.background} alt="Background do Perfil" />
+                <ProfileUser>
+                    <ProfileAvatar src={user.avatar} alt={`Foto de ${user.name}`} />
+                    <h2>{user.name}</h2>
+                    <h3>@{user.username}</h3>
+                </ProfileUser>
+                <ProfileAction>
+                    <ProfileAddIcon>
+                    <i className="bi bi-plus-square"></i>
+                    </ProfileAddIcon>
+                </ProfileAction>
+            </ProfileHeader>
+        </ProfileContainer>
     )
 };
 
---
+++ src/pages/Profile/ProfileStyled.jsx
@@ -0,0 +1,87 @@
+import styled from "styled-components";
+
+export const ProfileContainer = styled.section`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const ProfileHeader = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+    width: 80%;
+    margin-top: 1rem;
+    position: relative;
+    box-shadow: rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px;
+    border-radius: .3rem;
+    background-color:#FFF;
+    z-index: 0;
+`;
+
+export const ProfileEditIcon = styled.i`
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+    color: var(--cor-principal);
+    background-color: #FFF;
+    padding: .5rem;
+    border-radius: 50%;
+    font-size: 1.5rem;
+    cursor: pointer;
+    transition: all .3s ease-in-out;
+
+    &:hover{
+        color: var(--cor-terciaria);
+        background-color: #FFF;
+        font-size: 2rem;
+    }
+`;
+
+export const ProfileBackground = styled.img`
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    object-fit: cover;
+    width: 100%;
+    height: 60%;
+    z-index: -1;
+    border-radius: .3rem .3rem 0 0;
+`;
+
+export const ProfileUser = styled.div`
+    padding: 2rem;
+`;
+
+export const ProfileAvatar = styled.img`
+    border-radius: 50%;
+    width: 12rem;
+    height: 12rem;
+    border: solid 5px #FFF;
+    object-fit: cover;
+    object-position: center;
+`;
+
+export const ProfileAction = styled.div`
+    padding: 2rem;
+`;
+
+export const ProfileAddIcon = styled.i`
+    background-color: transparent;
+    border-radius: 50%;
+    color: var(--cor-principal);
+    outline: none;
+    border: none;
+    cursor: pointer !important;
+    font-weight: bold;
+    font-size: 2rem;
+    transition: all .3s ease-in-out;
+
+    &:hover{
+        color: var(--cor-terciaria);
+        font-size: 2.1rem;
+    }
+`;
